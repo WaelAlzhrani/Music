b6916e263548d43950d74caffe757480af0ea1b6
Glitch (hello-express) <none>
2019-07-31 23:25:05 +0000

    👫🎰 Checkpoint ./package.json:737800/2134 ./server.js:737800/1008 ./config.txt:737800/215

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
-// server.js
-// where your node app starts
-
-// init project
+const http = require('http');
 const express = require('express');
+const cmd = require('node-cmd');
 const app = express();
-
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/", (request, response) => {
+    response.sendStatus(200);
 });
+app.listen(process.env.PORT);
+setInterval(() => {
+    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+cmd.run('java jmusic.jar');
