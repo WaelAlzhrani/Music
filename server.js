26b05098de6a83a6689d450341abb0bd56668595
Glitch (dark-mu1) <none>
2019-11-02 17:58:08 +0000

    🎁🏅 Checkpoint

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-const http = require('http');
-const express = require('express');
-const cmd = require('node-cmd');
+const http = require("http");
+const express = require("express");
+const cmd = require("node-cmd");
 const app = express();
-app.get("/", (request, response) => {
-    response.sendStatus(200);
-});
+
+app.get("/", (_, r) => r.sendStatus(200));
 app.listen(process.env.PORT);
 setInterval(() => {
-    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 280000);
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 4 * 60 * 1000);
 
-cmd.run('java -Dnogui=true -jar jmusic.jar');
+cmd.run("java -Dnogui=true -jar jmusic.jar");
